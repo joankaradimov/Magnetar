e3423a85e4df60da17fae6ebdd6b940eedfccd6f
Joan Karadimov <joan.karadimov@gmail.com>
2022-04-11 00:56:34 +0300

    Do not rebuild MagnetarDat.mpq unless needed

--- MpqBuilder/main.cpp
+++ MpqBuilder/main.cpp
@@ -19,17 +19,22 @@ int main(int argc, char** argv)
     std::filesystem::path input_path = argv[1];
     std::filesystem::path output_path = "MagnetarDat.mpq";
     std::vector<std::filesystem::path> file_paths;
+    std::filesystem::file_time_type last_input_change = std::filesystem::file_time_type::min();
 
     for (const auto& entry : std::filesystem::recursive_directory_iterator(input_path))
     {
         // TODO: handle symlinks, maybe?
         if (entry.is_regular_file())
         {
+            last_input_change = std::max(last_input_change, entry.last_write_time());
             file_paths.push_back(entry.path());
         }
     }
 
-    // TODO: check if the MPQ needs rebuilding
+    if (std::filesystem::exists(output_path) && last_input_change < std::filesystem::last_write_time(output_path))
+    {
+        return 0;
+    }
 
     remove(output_path.generic_string().c_str());
 
