843af3e83e7b173879286fa0055fe5332f3f3aef
Joan Karadimov <joan.karadimov@gmail.com>
2022-04-14 01:01:18 +0200

    Rewrite a storm.dll initialization hack

--- Magnetorm/dllmain.cpp
+++ Magnetorm/dllmain.cpp
@@ -1,11 +1,18 @@
-#include "magnetorm.h"
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+extern "C" BOOL StormLibFileDestroy();
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		ReinitializeStormFileIO();
+		// An initial call to SFileDestroy is needed to avoid corruptions in SFile* functions
+		// see: https://github.com/bwapi/bwapi/issues/375#issuecomment-233162808
+		StormLibFileDestroy();
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
--- Magnetorm/magnetorm.cpp
+++
@@ -1,9 +0,0 @@
-#include "targetver.h"
-#include "magnetorm.h"
-
-void ReinitializeStormFileIO()
-{
-	HINSTANCE storm_dll = LoadLibrary(L"storm.dll");
-	void** storm_1505E5E4 = (void**)((char*)storm_dll + 0x5E5E4);
-	*storm_1505E5E4 = 0;
-}
