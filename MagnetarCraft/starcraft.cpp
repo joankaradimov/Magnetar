5077ad3dc5d01198fd92da5d8b99e919be47fe79
Joan Karadimov <joan.karadimov@gmail.com>
2022-07-25 22:28:06 +0300

    Reimplement LoadRaceSFX/LoadRaceUI

--- MagnetarCraft/starcraft.cpp
+++ MagnetarCraft/starcraft.cpp
@@ -3849,6 +3849,175 @@ int SinglePlayerMeleeInitGame_()
 
 FAIL_STUB_PATCH(SinglePlayerMeleeInitGame);
 
+int COMMON_SFX_COUNT = 113;
+int TERRAN_SFX_COUNT = 299;
+int PROTOSS_SFX_COUNT = 282;
+int ZERG_SFX_COUNT = 213;
+
+void LoadRaceSFX_(char a1)
+{
+	dword_64086C = 0;
+	dword_640870 = 0;
+	dword_640874 = 0;
+	dword_640878 = 0;
+	dword_64087C = 0;
+	selectionSoundCounter = 0;
+
+	if (dword_6D5BDC == 0)
+	{
+		for (int i = 0; i < COMMON_SFX_COUNT; ++i)
+		{
+			protoss_sfx_related[i] = i + 1;
+			terran_sfx_related[i] = i + 1;
+			zerg_sfx_related[i] = i + 1;
+		}
+
+		for (int i = 0; i < TERRAN_SFX_COUNT; ++i)
+		{
+			terran_sfx_related[COMMON_SFX_COUNT + i] = i + 174;
+		}
+		terran_sfx_related[COMMON_SFX_COUNT + TERRAN_SFX_COUNT] = 0;
+
+		for (int i = 0; i < PROTOSS_SFX_COUNT; ++i)
+		{
+			protoss_sfx_related[COMMON_SFX_COUNT + i] = i + 473;
+		}
+		protoss_sfx_related[COMMON_SFX_COUNT + PROTOSS_SFX_COUNT] = 0;
+
+		for (int i = 0; i < ZERG_SFX_COUNT; ++i)
+		{
+			zerg_sfx_related[COMMON_SFX_COUNT + i] = i + 755;
+		}
+		zerg_sfx_related[COMMON_SFX_COUNT + ZERG_SFX_COUNT] = 0;
+
+		dword_6D5BDC = 1;
+	}
+
+	playsound_init_UI(a1 ? off_513628[consoleIndex] : 0);
+}
+
+FAIL_STUB_PATCH(LoadRaceSFX);
+
+enum ExpandedMapData : u16
+{
+	EMD_none = 0x0,
+	EMD_swtutorial = 0x1,
+	EMD_swterran01 = 0x2,
+	EMD_swterran02 = 0x3,
+	EMD_swterran03 = 0x4,
+	EMD_swterran04 = 0x5,
+	EMD_swterran05 = 0x6,
+	EMD_tutorial = 0x7,
+	EMD_terran01 = 0x8,
+	EMD_terran02 = 0x9,
+	EMD_terran03 = 0xA,
+	EMD_terran04 = 0xB,
+	EMD_terran05 = 0xC,
+	EMD_terran06 = 0xD,
+	EMD_terran07 = 0xE,
+	EMD_terran08 = 0xF,
+	EMD_terran09 = 0x10,
+	EMD_terran10 = 0x11,
+	EMD_terran11 = 0x12,
+	EMD_terran12 = 0x13,
+	EMD_zerg01 = 0x14,
+	EMD_zerg02 = 0x15,
+	EMD_zerg03 = 0x16,
+	EMD_zerg04 = 0x17,
+	EMD_zerg05 = 0x18,
+	EMD_zerg06 = 0x19,
+	EMD_zerg07 = 0x1A,
+	EMD_zerg08 = 0x1B,
+	EMD_zerg09 = 0x1C,
+	EMD_zerg10 = 0x1D,
+	EMD_protoss01 = 0x1E,
+	EMD_protoss02 = 0x1F,
+	EMD_protoss03 = 0x20,
+	EMD_protoss04 = 0x21,
+	EMD_protoss05 = 0x22,
+	EMD_protoss06 = 0x23,
+	EMD_protoss07 = 0x24,
+	EMD_protoss08 = 0x25,
+	EMD_protoss09 = 0x26,
+	EMD_protoss10 = 0x27,
+	EMD_xprotoss01 = 0x28,
+	EMD_xprotoss02 = 0x29,
+	EMD_xprotoss03 = 0x2A,
+	EMD_xprotoss04 = 0x2B,
+	EMD_xprotoss05 = 0x2C,
+	EMD_xprotoss06 = 0x2D,
+	EMD_xprotoss07 = 0x2E,
+	EMD_xprotoss08 = 0x2F,
+	EMD_xterran01 = 0x30,
+	EMD_xterran02 = 0x31,
+	EMD_xterran03 = 0x32,
+	EMD_xterran04 = 0x33,
+	EMD_xterran05a = 0x34,
+	EMD_xterran05b = 0x35,
+	EMD_xterran06 = 0x36,
+	EMD_xterran07 = 0x37,
+	EMD_xterran08 = 0x38,
+	EMD_xzerg01 = 0x39,
+	EMD_xzerg02 = 0x3A,
+	EMD_xzerg03 = 0x3B,
+	EMD_xzerg04a = 0x3C,
+	EMD_xzerg04b = 0x3D,
+	EMD_xzerg04c = 0x3E,
+	EMD_xzerg04d = 0x3F,
+	EMD_xzerg04e = 0x40,
+	EMD_xzerg04f = 0x41,
+	EMD_xzerg05 = 0x42,
+	EMD_xzerg06 = 0x43,
+	EMD_xzerg07 = 0x44,
+	EMD_xzerg08 = 0x45,
+	EMD_xzerg09 = 0x46,
+	EMD_xbonus = 0x47,
+	EMD_xzerg10 = 0x48,
+	EMD_Unknown = 0x49,
+};
+
+void LoadRaceUI_()
+{
+	LoadRaceSFX_(1);
+	if (consoleIndex == Race::RACE_Zerg)
+	{
+		DlgGrp_Constructor(173, "Starcraft\\SWAR\\lang\\game.cpp", "dlgs\\zerg.grp", LoadGraphic);
+		currentMusicId = MT_ZERG1;
+	}
+	else if (consoleIndex == Race::RACE_Terran)
+	{
+		DlgGrp_Constructor(178, "Starcraft\\SWAR\\lang\\game.cpp", "dlgs\\terran.grp", LoadGraphic);
+		currentMusicId = MT_TERRAN1;
+	}
+	else if (consoleIndex == Race::RACE_Protoss)
+	{
+		DlgGrp_Constructor(183, "Starcraft\\SWAR\\lang\\game.cpp", "dlgs\\protoss.grp", LoadGraphic);
+		currentMusicId = MT_PROTOSS1;
+	}
+
+	if (CampaignIndex)
+	{
+		int v2 = CampaignIndex > ExpandedMapData::EMD_protoss10 ? xCampaignFirstMission[consoleIndex] : campaignFirstMission[consoleIndex];
+		int v1 = ((unsigned __int16)CampaignIndex - v2) % 3;
+		currentMusicId += v1;
+	}
+	else
+	{
+		int v0 = consoleRaceSpecific == -1 ? rand() % 3 : consoleRaceSpecific;
+		consoleRaceSpecific = v0 + 1;
+		if (consoleRaceSpecific >= 3)
+		{
+			consoleRaceSpecific = 0;
+		}
+		else
+		{
+			currentMusicId += consoleRaceSpecific;
+		}
+	}
+}
+
+FAIL_STUB_PATCH(LoadRaceUI);
+
 void setup_HUD_();
 
 int LoadGameCore_()
@@ -3987,7 +4156,7 @@ signed int LoadGameInit_()
 	randomCountsTotal = 0;
 	LastRandomNumber = initialSeed;
 	srand(initialSeed);
-	LoadRaceUI();
+	LoadRaceUI_();
 	hotkeyRemapping();
 	if (!GameInit_())
 		return 0;
@@ -4495,7 +4664,7 @@ void DestroyGame_()
 	stopSounds();
 	stopMusic();
 	stopSounds();
-	LoadRaceSFX(0);
+	LoadRaceSFX_(0);
 	InitializeInputProcs();
 	if (pylon_power_mask)
 	{
@@ -7954,84 +8123,6 @@ int CreateCampaignGame_(MapData mapData)
 }
 
 FAIL_STUB_PATCH(CreateCampaignGame);
-
-enum ExpandedMapData : u16
-{
-	EMD_none = 0x0,
-	EMD_swtutorial = 0x1,
-	EMD_swterran01 = 0x2,
-	EMD_swterran02 = 0x3,
-	EMD_swterran03 = 0x4,
-	EMD_swterran04 = 0x5,
-	EMD_swterran05 = 0x6,
-	EMD_tutorial = 0x7,
-	EMD_terran01 = 0x8,
-	EMD_terran02 = 0x9,
-	EMD_terran03 = 0xA,
-	EMD_terran04 = 0xB,
-	EMD_terran05 = 0xC,
-	EMD_terran06 = 0xD,
-	EMD_terran07 = 0xE,
-	EMD_terran08 = 0xF,
-	EMD_terran09 = 0x10,
-	EMD_terran10 = 0x11,
-	EMD_terran11 = 0x12,
-	EMD_terran12 = 0x13,
-	EMD_zerg01 = 0x14,
-	EMD_zerg02 = 0x15,
-	EMD_zerg03 = 0x16,
-	EMD_zerg04 = 0x17,
-	EMD_zerg05 = 0x18,
-	EMD_zerg06 = 0x19,
-	EMD_zerg07 = 0x1A,
-	EMD_zerg08 = 0x1B,
-	EMD_zerg09 = 0x1C,
-	EMD_zerg10 = 0x1D,
-	EMD_protoss01 = 0x1E,
-	EMD_protoss02 = 0x1F,
-	EMD_protoss03 = 0x20,
-	EMD_protoss04 = 0x21,
-	EMD_protoss05 = 0x22,
-	EMD_protoss06 = 0x23,
-	EMD_protoss07 = 0x24,
-	EMD_protoss08 = 0x25,
-	EMD_protoss09 = 0x26,
-	EMD_protoss10 = 0x27,
-	EMD_xprotoss01 = 0x28,
-	EMD_xprotoss02 = 0x29,
-	EMD_xprotoss03 = 0x2A,
-	EMD_xprotoss04 = 0x2B,
-	EMD_xprotoss05 = 0x2C,
-	EMD_xprotoss06 = 0x2D,
-	EMD_xprotoss07 = 0x2E,
-	EMD_xprotoss08 = 0x2F,
-	EMD_xterran01 = 0x30,
-	EMD_xterran02 = 0x31,
-	EMD_xterran03 = 0x32,
-	EMD_xterran04 = 0x33,
-	EMD_xterran05a = 0x34,
-	EMD_xterran05b = 0x35,
-	EMD_xterran06 = 0x36,
-	EMD_xterran07 = 0x37,
-	EMD_xterran08 = 0x38,
-	EMD_xzerg01 = 0x39,
-	EMD_xzerg02 = 0x3A,
-	EMD_xzerg03 = 0x3B,
-	EMD_xzerg04a = 0x3C,
-	EMD_xzerg04b = 0x3D,
-	EMD_xzerg04c = 0x3E,
-	EMD_xzerg04d = 0x3F,
-	EMD_xzerg04e = 0x40,
-	EMD_xzerg04f = 0x41,
-	EMD_xzerg05 = 0x42,
-	EMD_xzerg06 = 0x43,
-	EMD_xzerg07 = 0x44,
-	EMD_xzerg08 = 0x45,
-	EMD_xzerg09 = 0x46,
-	EMD_xbonus = 0x47,
-	EMD_xzerg10 = 0x48,
-	EMD_Unknown = 0x49,
-};
 
 struct __declspec(align(2)) ExpandedCampaignMenuEntry
 {
