55236381d6af1e7c15e61cfa46c95e513b84f0e0
Joan Karadimov <joan.karadimov@gmail.com>
2022-09-21 12:22:23 +0300

    Inline StartMagnetar in the test executable

--- MagnetarCraft/include/MagnetarCraft.h
+++ MagnetarCraft/include/MagnetarCraft.h
@@ -1,2 +1,5 @@
+void init_stacraftexe_clib();
+int VerifySystemMemory_();
+void FastIndexInit_();
 void report_error(const char* error_message);
 void StartMagnetar();
--- MagnetarCraft/test/tests.cpp
+++ MagnetarCraft/test/tests.cpp
@@ -1,10 +1,28 @@
-#include <exception>
 #include <Windows.h>
 
+#include "starcraft_executable.h"
+#include "magnetorm.h"
 #include "MagnetarCraft.h"
+#include "patching.h"
+#include "starcraft.h"
 
 int main()
 {
-	StartMagnetar();
+	// TODO: remove hard-coded paths
+	SetDllDirectoryA("C:\\games\\Starcraft-1.16");
+	StarCraftExecutable* starcraft_exe = new StarCraftExecutable("C:\\games\\Starcraft-1.16\\StarCraft.exe");
+	starcraft_exe->check();
+	SFileDestroy();
+
+	init_stacraftexe_clib();
+	BasePatch::apply_pending_patches();
+
+	hInst = starcraft_exe->GetModule();
+	main_thread_id = GetCurrentThreadId();
+	localDll_Init_(hInst);
+	FastIndexInit_();
+	BWSetSecurityInfo();
+	GameMainLoop_();
+
 	return 0;
 }
