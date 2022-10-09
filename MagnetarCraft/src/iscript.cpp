399b623d803eac4e0a774a9da2025d1800ef51be
Joan Karadimov <joan.karadimov@gmail.com>
2022-10-10 00:30:47 +0300

    Drop yet another temp var from BWFXN_PlayIscript

--- MagnetarCraft/src/iscript.cpp
+++ MagnetarCraft/src/iscript.cpp
@@ -36,7 +36,6 @@ void BWFXN_PlayIscript_(CImage* image, IScriptProgram* program_state, int noop, 
     unsigned __int16 v44; // bx
     CThingy* v46; // eax
     unsigned __int16 v47; // bx
-    CImage* v48; // edx
     unsigned __int16 v50; // ax
     char* v51; // edi
     char v52; // cl
@@ -379,9 +378,8 @@ void BWFXN_PlayIscript_(CImage* image, IScriptProgram* program_state, int noop, 
             {
                 continue;
             }
-            v48 = image;
             ISCRIPT_UseLOFile(&v125, image, *(v5 - 1), 0);
-            if (CThingy* thingy = ISCRIPT_CreateSprite(v48, v47, v125.x, v125.y, v48->spriteOwner->elevationLevel + 1))
+            if (CThingy* thingy = ISCRIPT_CreateSprite(image, v47, v125.x, v125.y, image->spriteOwner->elevationLevel + 1))
             {
                 if (image->flags & ImageFlags::IF_HORIZONTALLY_FLIPPED)
                 {
