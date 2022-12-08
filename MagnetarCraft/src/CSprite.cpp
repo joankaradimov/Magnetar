aa8156ccac3afb3de14155a78ef8c93a38e6b324
Joan Karadimov <joan.karadimov@gmail.com>
2022-12-08 15:53:26 +0300

    Reimplement writeSprites/ReadSpritesArray

---
+++ MagnetarCraft/src/CSprite.cpp
@@ -0,0 +1,200 @@
+#include "CSprite.h"
+#include "starcraft.h"
+#include "patching.h"
+#include "magnetorm.h"
+
+int __stdcall ReadSpritesArray_(FILE* a1)
+{
+    u16 v19;
+    if (fread(&v19, 2, 1, a1) != 1)
+    {
+        return 0;
+    }
+    memset(SpriteTable, 0, sizeof(SpriteTable));
+    if (v19)
+    {
+        int v17 = 38 * v19;
+        unsigned __int16* v4 = (unsigned __int16*)SMemAlloc(v17, "Starcraft\\SWAR\\lang\\CSprite.cpp", 1860, 0);
+        unsigned __int16* v18 = v4;
+        memset(v4, 0, v17);
+        if (!DecompressRead(v4, v17, a1))
+        {
+            SMemFree(v4, "Starcraft\\SWAR\\lang\\CSprite.cpp", 1866, 0);
+            return 0;
+        }
+        int v6 = v19;
+        while (v6)
+        {
+            const void* v7 = v4 + 1;
+            CSprite* v8 = &SpriteTable[*v4];
+            v4 += 19;
+            --v6;
+            memcpy(v8, v7, sizeof(CSprite));
+        }
+        SMemFree(v18, "Starcraft\\SWAR\\lang\\CSprite.cpp", 1884, 0);
+    }
+    for (int i = 0; i < _countof(SpriteTable); i++)
+    {
+        unpackSpriteData(SpriteTable + i);
+    }
+    initializeSpriteArray();
+    if (fread(SpritesOnTileRow.heads, 0x400, 1, a1) != 1)
+    {
+        return 0;
+    }
+    int v11 = map_size.height;
+    while (v11)
+    {
+        CSprite* v12 = SpritesOnTileRow.tails[v11-- + 255];
+        if (v12)
+        {
+            v12 = (CSprite*)&dword_629D74[9 * (_DWORD)v12];
+        }
+        SpritesOnTileRow.heads[v11] = v12;
+    }
+    if (fread(&SpritesOnTileRow, 0x400, 1, a1) != 1)
+    {
+        return 0;
+    }
+    int v13 = map_size.height;
+    while (v13)
+    {
+        CSprite* v14 = (CSprite*)dword_629284[v13--];
+        if (v14)
+        {
+            v14 = (CSprite*)&dword_629D74[9 * (_DWORD)v14];
+        }
+        SpritesOnTileRow.tails[v13] = v14;
+    }
+    int v15 = 0;
+    for (int i = 0; i < _countof(SpriteTable); ++i)
+    {
+        SpriteTable[i].index = v15++;
+    }
+    return 1;
+}
+
+FUNCTION_PATCH(ReadSpritesArray, ReadSpritesArray_);
+
+BOOL __stdcall writeSprites_(FILE* file)
+{
+    for (int i = 0; i < _countof(SpriteTable); i++)
+    {
+        packSpriteData(SpriteTable + i);
+    }
+    int v3 = 0;
+    CImage** v4 = &SpriteTable[1].pImageHead;
+    do
+    {
+        if (*(v4 - 9))
+        {
+            ++v3;
+        }
+        if (*v4)
+        {
+            ++v3;
+        }
+        if (v4[9])
+        {
+            ++v3;
+        }
+        if (v4[18])
+        {
+            ++v3;
+        }
+        if (v4[27])
+        {
+            ++v3;
+        }
+        v4 += 45;
+    } while ((int)v4 < (int)&byte_63FD30[56]);
+    size_t v6 = fwrite(&v3, 2, 1, file);
+    int v7 = v6 == 1;
+    if (v6 == 1 && (_WORD)v3)
+    {
+        int v8 = 38 * v3;
+        _WORD* v9 = (_WORD*) SMemAlloc(v8, "Starcraft\\SWAR\\lang\\CSprite.cpp", 1789, 0);
+        memset(v9, 0, v8);
+        void* location = v9;
+        for (int i = 0; i < _countof(SpriteTable); i++)
+        {
+            if (SpriteTable[i].pImageHead)
+            {
+                *v9 = i;
+                memcpy(v9 + 1, SpriteTable + i, sizeof(CSprite));
+                v9 += 19;
+            }
+        }
+        v7 = CompressWrite(location, v8, file);
+        SMemFree(location, "Starcraft\\SWAR\\lang\\CSprite.cpp", 1811, 0);
+    }
+    for (int i = 0; i < _countof(SpriteTable); i++)
+    {
+        unpackSpriteData(SpriteTable + i);
+    }
+    if (!v7)
+    {
+        return 0;
+    }
+    int v15 = map_size.height;
+    while (v15)
+    {
+        CSprite* v16 = SpritesOnTileRow.tails[v15 + 255];
+        v15--;
+        CSprite* v17;
+        if (v16)
+        {
+            v17 = (CSprite*)(v16 - SpriteTable + 1);
+        }
+        else
+        {
+            v17 = 0;
+        }
+        SpritesOnTileRow.heads[v15] = v17;
+    }
+    size_t v18 = fwrite(SpritesOnTileRow.heads, 0x400, 1, file);
+    int v19 = map_size.height;
+    int v20 = map_size.height;
+    BOOL result = v18 == 1;
+    while (v20)
+    {
+        CSprite* v22 = SpritesOnTileRow.tails[v20 + 255];
+        v20--;
+        if (v22)
+        {
+            v22 = (CSprite*)&dword_629D74[9 * (_DWORD)v22];
+        }
+        SpritesOnTileRow.heads[v20] = v22;
+    }
+    if (result)
+    {
+        for (; v19;)
+        {
+            int v23 = dword_629284[v19];
+            v19--;
+            if (v23)
+            {
+                SpritesOnTileRow.tails[v19] = (CSprite*)((v23 - (int)SpriteTable) / 0x24u + 1);
+            }
+            else
+            {
+                SpritesOnTileRow.tails[v19] = 0;
+            }
+        }
+        size_t v25 = fwrite(&SpritesOnTileRow, 0x400, 1, file);
+        int v26 = map_size.height;
+        result = v25 == 1;
+        while (v26)
+        {
+            CSprite* v27 = (CSprite*)dword_629284[v26--];
+            if (v27)
+            {
+                v27 = (CSprite*)&dword_629D74[9 * (_DWORD)v27];
+            }
+            SpritesOnTileRow.tails[v26] = v27;
+        }
+    }
+    return result;
+}
+
+FUNCTION_PATCH(writeSprites, writeSprites_);
