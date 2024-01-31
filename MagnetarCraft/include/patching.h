a3abe92765a055deab2fb67e5c9c628b886aaba9
Joan Karadimov <joan.karadimov@gmail.com>
2024-01-31 19:27:19 +0200

    Simplify FAIL_STUB_PATCH

--- MagnetarCraft/include/patching.h
+++ MagnetarCraft/include/patching.h
@@ -7,14 +7,11 @@
 #define CONCAT_IMPL3(x, y, z) x ## _ ## y ## _ ## z
 #define CONCAT3(x, y, z) CONCAT_IMPL3(x, y, z)
 
-#define CONCAT_IMPL4(a, b, c, d) a ## _ ## b ## _ ## c ## _ ## d
-#define CONCAT4(a, b, c, d) CONCAT_IMPL4(a, b, c, d)
-
 #define FUNCTION_PATCH(destination, replacement, tag, __VA_ARGS__) \
         FunctionPatch CONCAT3(function_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, replacement, {tag, __VA_ARGS__})
 
 #define FAIL_STUB_PATCH(destination, tag, __VA_ARGS__) \
-        FailStubPatch CONCAT4(fail_stub_patch, destination, __BASE_FILE__, __LINE__)(__FILE__, __LINE__, destination, {tag, __VA_ARGS__})
+        FailStubPatch CONCAT3(fail_stub_patch, destination, __LINE__)(__FILE__, __LINE__, destination, {tag, __VA_ARGS__})
 
 #define CALL_SITE_PATCH(destination, function, tag, __VA_ARGS__) \
         CallSitePatch CONCAT3(call_site_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, function, {tag, __VA_ARGS__})
