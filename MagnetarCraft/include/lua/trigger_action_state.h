ae3213779f246879619874285375d1cc65af0c5f
Joan Karadimov <joan.karadimov@gmail.com>
2024-04-05 21:44:13 +0300

    Improve the global Lua print function
    - add an overload that can print any Lua object
    - make 'print' an instance method of TriggerActionState

--- MagnetarCraft/include/lua/trigger_action_state.h
+++ MagnetarCraft/include/lua/trigger_action_state.h
@@ -9,6 +9,8 @@ public:
     TriggerActionState();
 
     void script(std::string_view code);
+    void print(const sol::object& obj);
+    void print(const std::string& message);
 
 private:
     sol::state state;
--- MagnetarCraft/src/lua/trigger_action_state.cpp
+++ MagnetarCraft/src/lua/trigger_action_state.cpp
@@ -2,21 +2,30 @@
 
 #include "starcraft.h"
 
-void lua_print(const char* message)
-{
-	if (active_trigger_player == g_LocalNationID && message)
-	{
-		int display_time = getTextDisplayTime_(message);
-		PrintText(message, 2, display_time + GetTickCount(), 1);
-	}
-}
-
 TriggerActionState::TriggerActionState()
 {
-	state.set_function("print", lua_print);
+	state.set_function("print", [this](sol::object obj) {
+		print(obj);
+	});
 }
 
 void TriggerActionState::script(std::string_view code)
 {
 	state.script(code);
+}
+
+void TriggerActionState::print(const sol::object& obj)
+{
+	// TODO: this may include addresss; fix this indeterminism
+	std::string message = state["tostring"](obj);
+	print(message);
+}
+
+void TriggerActionState::print(const std::string& message)
+{
+	if (active_trigger_player == g_LocalNationID && !message.empty())
+	{
+		int display_time = getTextDisplayTime_(message.c_str());
+		PrintText(message.c_str(), 2, display_time + GetTickCount(), 1);
+	}
 }
