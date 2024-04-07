ae33a03ff137027859748dae711f7e6bff6bce56
Joan Karadimov <joan.karadimov@gmail.com>
2024-04-07 12:49:42 +0300

    Enable the Lua trigger actions

--- MagnetarCraft/src/starcraft.cpp
+++ MagnetarCraft/src/starcraft.cpp
@@ -22708,7 +22708,7 @@ ActionPointer ActionTable_[] = {
 	TriggerAction_SetAllianceStatus_,
 	TriggerAction_DisableDebugMode_,
 	TriggerAction_EnableDebugMode_,
-	// TriggerAction_ExecuteLua,
+	TriggerAction_ExecuteLua,
 };
 
 void ExecuteTriggerActions_(TriggerListEntry* a1)
