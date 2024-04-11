0da00545e61fc6f1795ec129d25fdae41fe52c60
Joan Karadimov <joan.karadimov@gmail.com>
2024-04-12 00:24:25 +0300

    Fix warnings in the patching macros

--- MagnetarCraft/include/patching.h
+++ MagnetarCraft/include/patching.h
@@ -7,17 +7,17 @@
 #define CONCAT_IMPL3(x, y, z) x ## _ ## y ## _ ## z
 #define CONCAT3(x, y, z) CONCAT_IMPL3(x, y, z)
 
-#define FUNCTION_PATCH(destination, replacement, tag, __VA_ARGS__) \
-        FunctionPatch CONCAT3(function_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, replacement, {tag, __VA_ARGS__})
+#define FUNCTION_PATCH(destination, replacement, __VA_ARGS__) \
+        FunctionPatch CONCAT3(function_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, replacement, {__VA_ARGS__})
 
-#define FAIL_STUB_PATCH(destination, tag, __VA_ARGS__) \
-        FailStubPatch CONCAT3(fail_stub_patch, destination, __LINE__)(__FILE__, __LINE__, destination, {tag, __VA_ARGS__})
+#define FAIL_STUB_PATCH(destination, __VA_ARGS__) \
+        FailStubPatch CONCAT3(fail_stub_patch, destination, __LINE__)(__FILE__, __LINE__, destination, {__VA_ARGS__})
 
-#define CALL_SITE_PATCH(destination, function, tag, __VA_ARGS__) \
-        CallSitePatch CONCAT3(call_site_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, function, {tag, __VA_ARGS__})
+#define CALL_SITE_PATCH(destination, function, __VA_ARGS__) \
+        CallSitePatch CONCAT3(call_site_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, function, {__VA_ARGS__})
 
-#define NOP_PATCH(destination, block_length, tag, __VA_ARGS__) \
-        NopPatch CONCAT3(nop_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, block_length, {tag, __VA_ARGS__})
+#define NOP_PATCH(destination, block_length, __VA_ARGS__) \
+        NopPatch CONCAT3(nop_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, block_length, {__VA_ARGS__})
 
-#define MEMORY_PATCH(destination, data, tag, __VA_ARGS__) \
-        MemoryPatch CONCAT3(memory_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, data, {tag, __VA_ARGS__})
+#define MEMORY_PATCH(destination, data, __VA_ARGS__) \
+        MemoryPatch CONCAT3(memory_patch, __LINE__, __COUNTER__)(__FILE__, __LINE__, destination, data, {__VA_ARGS__})
